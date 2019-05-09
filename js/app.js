3f8d1c1312713f12cd4eb08da261b724947da854
AntAllch <anthonyallchorne@gmail.com>
2019-05-09 22:13:58 +0100

    Updated Mobile Nav

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
     $(".hamburger").click(function(){
-        $(".mobile-nav-menu").css({"transform": "translateY(0px)", "visibility": "visible", "opacity": "1"});
+        $(".mobile-nav-menu").css("top", "0px");
         $(".mobile-nav-menu li").hide().delay(500).fadeIn(500);
         $(".close-menu").hide().delay(200).fadeIn(400);
     });
 
     $(".close-menu").click(function(){
-        $(".mobile-nav-menu").delay(400).css({"transform": "translateY(-600px)", "visibility": "visible"});
+        $(".mobile-nav-menu").delay(400).css("top", "-600px");
     });
 
     $(".mobile-nav-links li").click(function(){
